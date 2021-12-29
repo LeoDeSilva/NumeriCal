2eb9668386baebe511fd21be2a4c095a376229e9
Leo De Silva <leo.desilvafamily@gmail.com>
2021-12-29 18:39:15 +0000

    Unit Node, String Node, Float, RRAPEN in pratt parser

--- parser/parser.go
+++ parser/parser.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"errors"
+	"fmt"
 	"numerical/lexer"
 	"strconv"
 )
@@ -32,22 +33,6 @@ func (e *ProgramNode) String() string {
 	return repr
 }
 
-type IdentifierNode struct {
-	Type       string
-	Identifier string
-}
-
-func (i *IdentifierNode) Eval() Node     { return i }
-func (i *IdentifierNode) String() string { return "{IDENTIFIER_NODE:" }
-
-type IntNode struct {
-	Type  string
-	Value int
-}
-
-func (i *IntNode) Eval() Node     { return i }
-func (i *IntNode) String() string { return "{INT_NODE:" + strconv.Itoa(i.Value) + "}" }
-
 type BinOpNode struct {
 	Type      string
 	Left      Node
@@ -57,8 +42,49 @@ type BinOpNode struct {
 
 func (b *BinOpNode) Eval() Node { return b }
 func (b *BinOpNode) String() string {
-	return "{BIN_OP_NODE:" + b.Left.String() + b.Operation + b.Right.String() + "}"
+	return "{" + b.Left.String() + ":" + b.Operation + ":" + b.Right.String() + "}"
 }
+
+type UnitNode struct {
+	Type  string
+	Value Node
+	Unit  string
+}
+
+func (u *UnitNode) Eval() Node     { return u }
+func (u *UnitNode) String() string { return u.Value.String() + u.Unit }
+
+type IdentifierNode struct {
+	Type       string
+	Identifier string
+}
+
+func (i *IdentifierNode) Eval() Node     { return i }
+func (i *IdentifierNode) String() string { return i.Identifier }
+
+type IntNode struct {
+	Type  string
+	Value int
+}
+
+func (i *IntNode) Eval() Node     { return i }
+func (i *IntNode) String() string { return strconv.Itoa(i.Value) }
+
+type FloatNode struct {
+	Type  string
+	Value float64
+}
+
+func (f *FloatNode) Eval() Node     { return f }
+func (f *FloatNode) String() string { return fmt.Sprintf("%v", f.Value) }
+
+type StringNode struct {
+	Type  string
+	Value string
+}
+
+func (s *StringNode) Eval() Node     { return s }
+func (s *StringNode) String() string { return "\"" + s.Value + "\"" }
 
 // ===========PARSER==========
 
@@ -117,31 +143,63 @@ func (p *Parser) parseExpr(rbp int) (Node, error) {
 		if err != nil {
 			return &ErrorNode{lexer.ERROR}, err
 		}
-		peekRbp = preference(p.peekToken().Type)
+		peekRbp = preference(p.token.Type)
 	}
 	return left, nil
 }
 
 func (p *Parser) parsePrefix() (Node, error) {
 	//Unit -> INT/FLOAT IDENTIFIER
-	//PAREN -> ( Expr )
 	//FUNCTION -> ID ( ... )
 	switch p.token.Type {
+	case lexer.LPAREN:
+		p.advance()
+		expression, err := p.parseExpr(preference(lexer.LPAREN))
+		if err != nil {
+			return &ErrorNode{lexer.ERROR}, err
+		}
+		p.advance()
+		return expression, nil
 	case lexer.INT:
 		value, err := strconv.Atoi(p.token.Literal)
 		if err != nil {
 			return &ErrorNode{lexer.ERROR}, err
 		}
 		p.advance()
-		return &IntNode{Type: lexer.INT_NODE, Value: value}, nil
-	case lexer.LPAREN:
-
+		node := &IntNode{Type: lexer.INT_NODE, Value: value}
+		if p.token.Type == lexer.IDENTIFIER {
+			unit := p.token.Literal
+			p.advance()
+			return &UnitNode{lexer.UNIT_NODE, node, unit}, nil
+		}
+		return node, nil
+	case lexer.FLOAT:
+		value, err := strconv.ParseFloat(p.token.Literal, 64)
+		if err != nil {
+			return &ErrorNode{lexer.ERROR}, err
+		}
+		p.advance()
+		node := &FloatNode{Type: lexer.INT_NODE, Value: value}
+		if p.token.Type == lexer.IDENTIFIER {
+			unit := p.token.Literal
+			p.advance()
+			return &UnitNode{lexer.UNIT_NODE, node, unit}, nil
+		}
+		return node, nil
+	case lexer.IDENTIFIER:
+		identifier := p.token.Literal
+		p.advance()
+		return &IdentifierNode{lexer.IDENTIFIER, identifier}, nil
+	case lexer.STRING:
+		value := p.token.Literal
+		p.advance()
+		return &StringNode{lexer.STRING_NODE, value}, nil
 	}
 	return &ErrorNode{lexer.ERROR}, errors.New("SyntaxError: parsePrefix() unsupported prefix:" + p.token.Literal)
 }
 
 func (p *Parser) parseInfix(left Node, operation string) (Node, error) {
-	if !contains([]string{"EE", "NE", "LT", "GT", "LTE", "GTE", "ADD", "SUB", "MUL", "DIV", "MOD", "POW"}, p.token.Type) {
+	if !contains([]string{"EE", "NE", "LT", "GT", "LTE", "GTE", "ADD", "SUB", "MUL", "DIV", "MOD", "POW", "IN", "ARROW"}, p.token.Type) {
 		return &ErrorNode{lexer.ERROR}, errors.New("SyntaxError: parseInfix() unsupported opperator:" + p.token.Literal)
 	}
 	p.advance()
@@ -149,27 +207,30 @@ func (p *Parser) parseInfix(left Node, operation string) (Node, error) {
 	if err != nil {
 		return &ErrorNode{lexer.ERROR}, err
 	}
-
+	if operation == lexer.ARROW {
+		operation = lexer.IN
+	}
 	return &BinOpNode{Type: lexer.BIN_OP_NODE, Left: left, Operation: operation, Right: right}, nil
 }
 
 func preference(tokenType string) int {
 	var preferences = map[string]int{
+		lexer.IN:     5,
+		lexer.ARROW:  5,
 		lexer.EE:     10,
 		lexer.NE:     10,
 		lexer.GT:     10,
 		lexer.GTE:    10,
 		lexer.LT:     10,
 		lexer.LTE:    10,
+		lexer.MOD:    15,
 		lexer.ADD:    20,
 		lexer.SUB:    20,
 		lexer.MUL:    30,
 		lexer.DIV:    30,
 		lexer.POW:    40,
-		lexer.IN:     50,
-		lexer.ARROW:  50,
 		lexer.LPAREN: 0,
-		lexer.EOF:    0,
+		lexer.RPAREN: -1,
 	}
 
 	if rbp, ok := preferences[tokenType]; ok {
---
+++ parser/parser_test.go
@@ -0,0 +1,82 @@
+package parser
+
+import (
+	"numerical/lexer"
+	"reflect"
+	"strconv"
+	"testing"
+)
+
+func Test_preference(t *testing.T) {
+	tests := []struct {
+		tokenType string
+		wantRes   int
+	}{
+		{lexer.ADD, 20},
+		{lexer.MUL, 30},
+		{lexer.EE, 10},
+		{lexer.EOF, 0},
+		{lexer.LPAREN, 0},
+		{lexer.POW, 40},
+		{lexer.ARROW, 5},
+		{lexer.IN, 5},
+	}
+	for i, tt := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			if got := preference(tt.tokenType); got != tt.wantRes {
+				t.Errorf("preference() = %v, want %v", got, tt.wantRes)
+			}
+		})
+	}
+}
+
+func Test_contains(t *testing.T) {
+	operations := []string{"EE", "NE", "LT", "GT", "LTE", "GTE", "ADD", "SUB", "MUL", "DIV", "MOD", "POW", "IN", "ARROW"}
+	tests := []struct {
+		array   []string
+		element string
+		wantRes bool
+	}{
+		{operations, "IN", true},
+		{operations, "ADD", true},
+		{operations, "MOD", true},
+		{operations, "EE", true},
+		{operations, "INT", false},
+		{operations, "NN", false},
+		{operations, "", false},
+	}
+	for i, tt := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			if got := contains(tt.array, tt.element); got != tt.wantRes {
+				t.Errorf("contains() = %v, want %v", got, tt.wantRes)
+			}
+		})
+	}
+}
+
+func TestParser_Parse(t *testing.T) {
+	tests := []struct {
+		program string
+		wantRes ProgramNode
+		wantErr bool
+	}{
+		{"1+2", ProgramNode{lexer.PROGRAM_NODE, []Node{&BinOpNode{lexer.BIN_OP_NODE, &IntNode{lexer.INT_NODE, 1}, lexer.ADD, &IntNode{lexer.INT_NODE, 2}}}}, false},
+		{"1-2", ProgramNode{lexer.PROGRAM_NODE, []Node{&BinOpNode{lexer.BIN_OP_NODE, &IntNode{lexer.INT_NODE, 1}, lexer.SUB, &IntNode{lexer.INT_NODE, 2}}}}, false},
+		{"(1+2)*3", ProgramNode{lexer.PROGRAM_NODE, []Node{&BinOpNode{lexer.BIN_OP_NODE, &BinOpNode{lexer.BIN_OP_NODE, &IntNode{lexer.INT_NODE, 1}, lexer.ADD, &IntNode{lexer.INT_NODE, 2}}, lexer.MUL, &IntNode{lexer.INT_NODE, 3}}}}, false},
+	}
+	for i, tt := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			l := lexer.NewLexer(tt.program)
+			tokens, err := l.Lex()
+			p := NewParser(tokens)
+			got, err := p.Parse()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Parser.Parse() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.wantRes) {
+				t.Errorf("Parser.Parse() = %v, want %v", got, tt.wantRes)
+			}
+		})
+	}
+}
